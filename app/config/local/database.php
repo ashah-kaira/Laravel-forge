aa30784f22591bb82c0da0a4c366c35bd67530ae
Adit Shah <ashah@kairasoftware.com>
2016-03-09 18:03:59 +0530

    Update database.php

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'host'      => '54.208.79.53',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'NkheseJdQUtYwfhJvvdg',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
